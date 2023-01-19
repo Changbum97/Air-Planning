f9c796c07dd7a1e47a183e23ef804793c204fece
Changbum97 <chb2005@naver.com>
2023-01-19 17:06:55 +0900

    FEAT: #6 임시로 화면 이동을 위한 PageController 생성

--- src/main/java/com/example/airplanning/controller/PageController.java
+++ src/main/java/com/example/airplanning/controller/PageController.java
@@ -1,2 +1,43 @@
-package com.example.airplanning.controller;public class PageController {
+package com.example.airplanning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/boards/detail")
+    public String boardDetail() {
+        return "boards/detail";
+    }
+
+    @GetMapping("/boards/write")
+    public String boardWrite() {
+        return "boards/write";
+    }
+
+    @GetMapping("/boards/list")
+    public String boardList() {
+        return "boards/list";
+    }
+
+    @GetMapping("/users/my-page")
+    public String myPage() {
+        return "users/myPage";
+    }
+
+    @GetMapping("/users/planner")
+    public String plannerPage() {
+        return "users/planner";
+    }
+
+    @GetMapping("/users/login")
+    public String loginPage() {
+        return "users/login";
+    }
+
+    @GetMapping("/users/join")
+    public String joinPage() {
+        return "users/join";
+    }
 }
