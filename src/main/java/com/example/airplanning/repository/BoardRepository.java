37721b3b456697afd1133d9577b18a859287ed07
pigeon2gugu <khn1135@gmial.com>
2023-01-20 16:21:53 +0900

    FEAT : #4 마이페이지 내 정보 보회 + 내가 작성한 게시글, 리뷰, 댓글 조회 - 내 정보 : name, birth, userName return - 게시글 : id, title, category return - 리뷰 : id, title return - 댓글 : id, commentType, content, parentId(게시글 or 리뷰 id) return

--- src/main/java/com/example/airplanning/repository/BoardRepository.java
+++ src/main/java/com/example/airplanning/repository/BoardRepository.java
@@ -1,7 +1,11 @@
 package com.example.airplanning.repository;
 
 import com.example.airplanning.domain.entity.Board;
+import com.example.airplanning.domain.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllByUser(User user, Pageable pageable);
 }
