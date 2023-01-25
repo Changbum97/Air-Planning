03c554fb7d7b2a851dab4a5c07e6d91a1d560f1b
Changbum97 <chb2005@naver.com>
2023-01-25 15:49:02 +0900

    FEAT: #23 리뷰 등록 기능 구현

--- src/main/java/com/example/airplanning/service/ReviewService.java
+++ src/main/java/com/example/airplanning/service/ReviewService.java
@@ -1,2 +1,28 @@
-package com.example.airplanning.service;public class ReviewService {
+package com.example.airplanning.service;
+
+import com.example.airplanning.domain.dto.review.ReviewCreateRequest;
+import com.example.airplanning.domain.entity.Review;
+import com.example.airplanning.domain.entity.User;
+import com.example.airplanning.exception.AppException;
+import com.example.airplanning.exception.ErrorCode;
+import com.example.airplanning.repository.ReviewRepository;
+import com.example.airplanning.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    public Long write(ReviewCreateRequest request, String userName) {
+
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUNDED));
+
+        Review savedReview = reviewRepository.save(request.toEntity(user));
+        return savedReview.getId();
+    }
 }
