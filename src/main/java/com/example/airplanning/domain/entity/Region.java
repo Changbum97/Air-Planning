e2f86dbef9a308dfe6a91006408abc46749d31c8
pigeon2gugu <khn1135@gmail.com>
2023-02-06 13:59:39 +0900

    FEAT: #54 Region table 추가

---
+++ src/main/java/com/example/airplanning/domain/entity/Region.java
@@ -0,0 +1,24 @@
+package com.example.airplanning.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Region extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String region1;     // 시/도
+    private String region2;       // 구/시
+
+}
