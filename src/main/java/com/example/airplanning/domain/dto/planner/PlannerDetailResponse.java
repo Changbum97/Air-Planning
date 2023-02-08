5288439e19bfa048da7289de0ce6f10278f013ed
pigeon2gugu <khn1135@gmail.com>
2023-02-08 17:09:28 +0900

    FEAT: #56 포토폴리오 상세 페이지 - 업로드 한 파일이 있는 경우, 다운로드 버튼 보이게 생성 - 플래너정보 페이지 등록한 프로필사진로 변경

--- src/main/java/com/example/airplanning/domain/dto/planner/PlannerDetailResponse.java
+++ src/main/java/com/example/airplanning/domain/dto/planner/PlannerDetailResponse.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class PlannerDetailResponse {
 
     private String userName;
+    private String image;
     private Double star;            // 별점 총 합 / 리뷰 개수
     private String region;          // 자신있는 지역
     private String description;     // 자기 소개
@@ -20,6 +21,7 @@ public class PlannerDetailResponse {
     public static PlannerDetailResponse of(Planner planner) {
         return PlannerDetailResponse.builder()
                 .userName(planner.getUser().getUserName())
+                .image(planner.getUser().getImage())
                 .star((double)planner.getStarSum() / planner.getReviewCount())
                 .region(planner.getRegion().getRegion1() +" "+ planner.getRegion().getRegion2())
                 .description(planner.getDescription())
