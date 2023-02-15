d3e5faf9cb12eb24162230c6d19f5379aee95b36
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2023-02-15 18:41:28 +0900

    error/planner_nickname

--- src/main/java/com/example/airplanning/domain/dto/planner/PlannerDetailResponse.java
+++ src/main/java/com/example/airplanning/domain/dto/planner/PlannerDetailResponse.java
@@ -18,6 +18,7 @@ public class PlannerDetailResponse {
     private Double star;            // 별점 총 합 / 리뷰 개수
     private String region;          // 자신있는 지역
     private String description;     // 자기 소개
+    private String nickname;    // 작성자 닉네임
 
     public static PlannerDetailResponse of(Planner planner) {
         Double star;
@@ -29,6 +30,7 @@ public class PlannerDetailResponse {
         return PlannerDetailResponse.builder()
                 .id(planner.getId())
                 .userName(planner.getUser().getUserName())
+                .nickname(planner.getUser().getNickname())
                 .image(planner.getUser().getImage())
                 .star(star)
                 .region(planner.getRegion().getRegion1() +" "+ planner.getRegion().getRegion2())
